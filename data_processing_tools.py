a859e4d3fcb1f7c8793a3d07fddd7139110cf57e
ajallal88 <ajallal88@gmail.cim>
2021-07-18 20:23:01 +0530

    changes made by Aqib Added scaling code.

--- data_processing_tools.py
+++ data_processing_tools.py
@@ -13,25 +13,24 @@ from sklearn.impute import SimpleImputer
 Imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 Imputer.fit(x[:, 1:3])
 x[:, 1:3] = Imputer.transform(x[:, 1:3])
-print(x)
 
 # Encoding the independent variable
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
 x = np.array(ct.fit_transform(x))
-print(x)
 
 # Encoding the dependent Variable
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 y = le.fit_transform(y)
-print(y)
 
 # Splitting the dataset into Training set and Test set
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=1)
-print(x_train)
-print(x_test)
-print(y_train)
-print(y_test)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train[:, 3:] = sc.fit_transform(x_train[:, 3:])
+x_test[:, 3:] = sc.transform(x_test[:, 3:])
