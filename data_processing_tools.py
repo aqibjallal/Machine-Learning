47fcb8addef2db0ee855a4578bd4851a6fba5cec
ajallal88 <ajallal88@gmail.cim>
2021-07-18 19:54:21 +0530

    changes made by Aqib Added splitting code.

--- data_processing_tools.py
+++ data_processing_tools.py
@@ -27,3 +27,11 @@ from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 y = le.fit_transform(y)
 print(y)
+
+# Splitting the dataset into Training set and Test set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=1)
+print(x_train)
+print(x_test)
+print(y_train)
+print(y_test)
