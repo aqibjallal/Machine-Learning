17f3ad87ce5ac57243d02c0e2e2c7df29d4125b4
ajallal88 <ajallal88@gmail.cim>
2021-07-24 18:43:06 +0530

    Adding the codes to split the dataset

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -10,6 +10,6 @@ y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=1/3, random_state=0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
 
